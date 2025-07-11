3ac5de719618eb699d923016e0c89fd916dc06c5
saiabhiteja <saiabhiteja29@gmail.com>
2025-07-11 21:53:28 +0530

    fix: remove Preact aliasing for Headless UI compatibility

--- next.config.js
+++ next.config.js
@@ -97,17 +97,7 @@ module.exports = withBundleAnalyzer({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     })
-
-    if (!dev && !isServer) {
-      // Replace React with Preact only in client production build
-      Object.assign(config.resolve.alias, {
-        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      })
-    }
-
+    // Removed Preact aliasing for production build
     return config
   },
 })
